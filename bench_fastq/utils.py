78d8d3544e470a8b4536b5883b562062e728bfc4
stharrold <samuel.harrold@gmail.com>
2014-09-26 15:19:37 -0500

    reoredered parsed dataframe

--- bench_fastq/utils.py
+++ bench_fastq/utils.py
@@ -6,6 +6,7 @@
 from __future__ import print_function, division, absolute_import
 import os
 import sys
+import pdb
 import json
 import datetime as dt
 import numpy as np
@@ -246,14 +247,10 @@ def parsed_dict_to_df(parsed):
             # Skip size_bytes for file.
             if isinstance(parsed[filename][iteration], dict):
                 for method in parsed[filename][iteration]:
-                    process_df_dict = {}
-                    for process in parsed[filename][iteration][method]:
-                        if isinstance(parsed[filename][iteration][method][process], dict):
-                            process_df_dict[process] = pd.DataFrame.from_dict(parsed[filename][iteration][method][process], orient='index')
-                    method_df_dict[method] = pd.concat(process_df_dict, axis=0)
-                iteration_df_dict[iteration] = pd.concat(method_df_dict, axis=0)
-        filename_df_dict[filename] = pd.concat(iteration_df_dict, axis=0)
-    parsed_df = pd.concat(filename_df_dict, axis=0)
-    parsed_df.index.names = ['filename', 'iteration', 'method', 'process', 'quantity']
-    parsed_df.columns = ['value']
+                    method_df_dict[method] = pd.DataFrame.from_dict(parsed[filename][iteration][method], orient='columns')
+                iteration_df_dict[iteration] = pd.concat(method_df_dict, axis=1)
+        filename_df_dict[filename] = pd.concat(iteration_df_dict, axis=1)
+    parsed_df = pd.concat(filename_df_dict, axis=1)
+    parsed_df.index.names = ['quantity']
+    parsed_df.columns.names = ['filename', 'iteration', 'method', 'process']
     return parsed_df
